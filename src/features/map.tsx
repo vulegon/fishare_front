d3dd821d7816721a0e99970b46bd538bb6aea2df
ikebata_yuki <walkurepqrt@gmail.com>
2023-06-21 07:17:15 +0900

    google-map導入

--- src/features/map.tsx
+++ src/features/map.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api'
+import { APIKey } from '../config/api_key'
 
 function map() {
+  const containerStyle = {
+    width: '400px',
+    height: '400px',
+  }
+
+  const center = {
+    lat: -3.745,
+    lng: -38.523,
+  }
   return (
-    <div>
-      
-    </div>
+    <LoadScript googleMapsApiKey={APIKey}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        {/* Child components, such as markers, info windows, etc. */}
+        <></>
+      </GoogleMap>
+    </LoadScript>
   )
 }
 
