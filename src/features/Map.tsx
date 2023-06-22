87688555a9c3914caa9d9c612c307fe2f5471fe5
ikebata_yuki <walkurepqrt@gmail.com>
2023-06-23 07:20:47 +0900

    航空写真だと駅名とかでないのでやっぱりデフォルトのままで。defaultMapOptionだけ変更

--- src/features/Map.tsx
+++ src/features/Map.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { GoogleMap, LoadScript } from '@react-google-maps/api'
 import { apiKey } from '../config/google_map/api_key'
-import { mapContainerStyle, center, zoom } from './defaultMapOption'
+import { mapContainerStyle, mapOptions } from './defaultMapOption'
 
 function Map() {
   return (
     <LoadScript googleMapsApiKey={apiKey}>
-      <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={zoom}>
+      <GoogleMap mapContainerStyle={mapContainerStyle} options={mapOptions}>
       </GoogleMap>
     </LoadScript>
   )
