30a9fe0e19c54ab4706f1ab9978c59db63986392
ikebata_yuki <walkurepqrt@gmail.com>
2023-06-23 06:55:52 +0900

    ブラウザ移動したときの画面サイズが調整されないのを修正

--- src/features/map.tsx
+++ src/features/map.tsx
@@ -1,24 +1,12 @@
 import React from 'react'
 import { GoogleMap, LoadScript } from '@react-google-maps/api'
 import { apiKey } from '../config/google_map/api_key'
+import { mapContainerStyle, center, zoom } from './defaultMapOption'
 
 function Map() {
-  const height = window.innerHeight;
-  document.getElementById('root')!.style.height = height + 'px';
-
-  const containerStyle = {
-    width: '100%',
-    height: height,
-  }
-
-  const center = {
-    lat: 36.063053704526226,
-    lng: 136.22288055523217,
-  }
-
   return (
     <LoadScript googleMapsApiKey={apiKey}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+      <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={zoom}>
       </GoogleMap>
     </LoadScript>
   )
