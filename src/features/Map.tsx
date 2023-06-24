f41beb88b9ac577f53114d554c0a253151ff42fa
ikebata_yuki <walkurepqrt@gmail.com>
2023-06-24 19:03:24 +0900

    ボタンの配置まで

--- src/features/Map.tsx
+++ src/features/Map.tsx
@@ -3,9 +3,11 @@ import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import { apiKey } from '../config/google_map/api_key';
 import { mapContainerStyle, mapOptions } from './defaultMapOption';
 import { MarkerPosition } from '../types/types';
+import RegisterButton from '../components/RegisterButton';
 
 function Map() {
   const [markerPosition, setMarkerPosition] = useState<MarkerPosition>({ lat: undefined, lng: undefined });
+  const [spotRegisterButtonIsDisabled, setSpotRegisterButtonIsDisabled] = useState<boolean>(true);
   const onMapClick = (e: google.maps.MapMouseEvent) => {
     if (e.latLng && e.latLng) {
       console.log(`緯度: ${e.latLng.lat()}, 経度: ${e.latLng.lng()}`);
@@ -13,14 +15,19 @@ function Map() {
         lat: e.latLng.lat(),
         lng: e.latLng.lng(),
       });
+      setSpotRegisterButtonIsDisabled(false);
     }
   };
+  
   return (
     <LoadScript googleMapsApiKey={apiKey}>
       <GoogleMap mapContainerStyle={mapContainerStyle} options={mapOptions} onClick={onMapClick}>
         {markerPosition.lat && markerPosition.lng && (
           <Marker position={{ lat: markerPosition.lat, lng: markerPosition.lng }} />
         )}
+        <div style={{ position: 'absolute', bottom: '20px', right: '70px' }}>
+          <RegisterButton isDisabled={spotRegisterButtonIsDisabled} />
+        </div>
       </GoogleMap>
     </LoadScript>
   );
