641b39a8f9d5ad5bccea6ddaa3a40569a31c6100
ikebata_yuki <walkurepqrt@gmail.com>
2023-06-24 21:42:05 +0900

    やっぱり全画面ボタン表示にする

--- src/features/defaultMapOption.ts
+++ src/features/defaultMapOption.ts
@@ -8,11 +8,9 @@ export const mapContainerStyle = {
 interface MapOptions {
   zoom: number;
   center: Position;
-  fullscreenControl: boolean;
 }
 
 export const mapOptions: MapOptions = {
   zoom: 15,
   center: { lat: 36.063053704526226, lng: 136.22288055523217 },
-  fullscreenControl: false,
 };
