281f3df8fa234648c32d6cebe0472db76db2f235
ikebata_yuki <walkurepqrt@gmail.com>
2023-08-05 10:43:56 +0900

    初期値のロード。マーカー設置まで

--- src/features/spots/SpotCreateFormMap.tsx
+++ src/features/spots/SpotCreateFormMap.tsx
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { GoogleMap, Marker } from '@react-google-maps/api';
 import { MarkerPosition } from '../../types/types';
+import { useLocation } from 'react-router-dom';
 
 function SpotCreateFormMap() {
   const [markerPosition, setMarkerPosition] = useState<MarkerPosition>({ lat: undefined, lng: undefined });
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const latParam = searchParams.get('lat');
+  const lngParam = searchParams.get('lng');
+  const centerLat = latParam ? parseFloat(latParam) : 36.063053704526226;
+  const centerLng = lngParam ? parseFloat(lngParam) : 136.22288055523217;
+
+  useEffect(() => {
+    setMarkerPosition({ lat: centerLat, lng: centerLng});
+   },[])
 
   const onMapClick = (e: google.maps.MapMouseEvent) => {
     if (e.latLng && e.latLng) {
@@ -22,7 +33,7 @@ function SpotCreateFormMap() {
       }}
       options={{
         zoom: 15,
-        center: { lat: 36.063053704526226, lng: 136.22288055523217 },
+        center: { lat: centerLat, lng: centerLng },
         fullscreenControl: false,
       }}
       onClick={onMapClick}
