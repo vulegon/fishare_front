2e758785121a58fbca546ada01865a308b4bd32d
ikebata_yuki <walkurepqrt@gmail.com>
2023-08-18 06:13:06 +0900

    釣り場取得完了

--- src/features/Map.tsx
+++ src/features/Map.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleMap, Marker } from '@react-google-maps/api';
 import { mapContainerStyle, mapOptions } from './defaultMapOption';
 import { MarkerPosition } from '../types/types';
 import SpotRegisterButton from './SpotRegisterButton';
 import Header from '../Header';
+import { getSpots } from '../services/apiClient';
 
 function Map() {
   const [markerPosition, setMarkerPosition] = useState<MarkerPosition>({ lat: undefined, lng: undefined });
@@ -18,11 +19,37 @@ function Map() {
       setSpotRegisterButtonIsDisabled(false);
     }
   };
+  interface Spot {
+    id: string;
+    lat: number;
+    lng: number;
+  }
+
+  const [spots, setSpots] = useState<Spot[]>([]);
+
+  const fetchSpots = async () => {
+    try {
+      const response = await getSpots();
+      if (response.status === 200) {
+        const data = await response.json();
+        setSpots(data.spots);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchSpots();
+  }, []);
 
   return (
     <div>
       <Header />
       <GoogleMap mapContainerStyle={mapContainerStyle()} options={mapOptions} onClick={onMapClick}>
+        {spots.map((spot) => (
+          <Marker key={ spot.id.toString()}  position={{ lat: spot.lat, lng: spot.lng }} />
+        ))}
         {markerPosition.lat && markerPosition.lng && (
           <Marker position={{ lat: markerPosition.lat, lng: markerPosition.lng }} />
         )}
--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,32 +1,43 @@
 const baseURL = process.env.REACT_APP_BASE_URL;
+type Headers = {
+  [key: string]: string;
+};
+const defaultHeaders = {
+  'Content-Type': 'application/json',
+};
 
-const createRequest = (endpoint: string, method: string, body?: any, argumentHeaders?: { [key: string]: string }) => {
-  const url = `${baseURL}/${endpoint}`;
-  type Headers = {
-    [key: string]: string;
-  };
+// const createRequest = (endpoint: string, method: string, body?: any, argumentHeaders?: { [key: string]: string }) => {
+//   const url = `${baseURL}/${endpoint}`;
 
-  const defaultHeaders = {
-    'Content-Type': 'application/json',
-  };
+//   const headers: Headers = argumentHeaders ? argumentHeaders : defaultHeaders;
 
-  const headers: Headers = argumentHeaders ? argumentHeaders : defaultHeaders;
+//   const options: RequestInit = {
+//     method,
+//     headers,
+//     body: body ? JSON.stringify(body) : undefined,
+//   };
 
+//   return fetch(url, options);
+// };
+
+// export async function apiClient(
+//   endpoint: string,
+//   method: string,
+//   body?: any,
+//   argumentHeaders?: { [key: string]: string }
+// ): Promise<Response> {
+//   const response = await createRequest(endpoint, method, body, argumentHeaders);
+//   return response;
+// }
+
+export const getSpots = async (): Promise<Response> => {
+  const url = `${baseURL}/spots`;
+  const method = 'GET';
+  const headers: Headers = defaultHeaders;
   const options: RequestInit = {
     method,
     headers,
-    body: body ? JSON.stringify(body) : undefined,
   };
-
-  return fetch(url, options);
-};
-
-export async function apiClient(
-  endpoint: string,
-  method: string,
-  body?: any,
-  argumentHeaders?: { [key: string]: string }
-): Promise<Response> {
-  const response = await createRequest(endpoint, method, body, argumentHeaders);
-  return response;
+  const response = await fetch(url, options);
+  return response
 };
